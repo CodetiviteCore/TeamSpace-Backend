fb0badbbdc96f1f0708a1b5dc4aa37c90a78368c
Alexandra ogubuike <ogubuikealex@gmail.com>
2022-09-11 22:12:27 +0100

    Added code files

--- src/utils/server.js
+++ src/utils/server.js
@@ -5,6 +5,7 @@ const expressPino = require("express-pino-logger");
 const logger = require("./logger");
 const routes = require("../routes");
 const { PORT } = require("../config/env");
+const {database} = require("../config/database");
 
 class SetupServer {
   app = express();
@@ -25,13 +26,17 @@ class SetupServer {
   async init() {
     this.setupExpress();
     this.setupControllers();
+    this.start();
     //must be the last
 
     this.setupErrorHandlers();
   }
 
+  //Step One
   setupExpress() {
+    database();
     this.app.use(express.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
     this.app.use(
       expressPino({
         logger,
@@ -42,12 +47,34 @@ class SetupServer {
         origin: "*",
       })
     );
+    app.use(async (req, res, next) => {
+      if (req.headers["x-access-token"]) {
+        const accessToken = req.headers["x-access-token"];
+        const { userId, exp } = await jwt.verify(
+          accessToken,
+          process.env.JWT_SECRET
+        );
+        // Check if token has expired
+        if (exp < Date.now().valueOf() / 1000) {
+          return res
+            .status(401)
+            .json({
+              error: "JWT token has expired, please login to obtain a new one",
+            });
+        }
+        res.locals.loggedInUser = await User.findById(userId);
+        next();
+      } else {
+        next();
+      }
+    });    
   }
 
+  //Step two
   setupControllers() {
     this.app.get("/", (req, res) =>
       res.status(200).send({
-        message: "Welcome to Property Pro",
+        message: "Welcome to Role Based Sytem",
       })
     );
     this.app.use("/v1.0/api", routes);
